81b4276c6c514324f4e1ff3f887b7995b06d1d7e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-02-02 00:08:53 +0100

    Velocity adds up

--- src/player.rs
+++ src/player.rs
@@ -37,7 +37,7 @@ fn move_player(
     mut player_query: Query<(&mut Transform, &mut Velocity), With<Player>>,
 ) {
     let rotation_speed = 2.0;
-    let translation_speed = 150.;
+    let thrust_force = 5.;
     // Seems confusing but "forward" is "up" in the 2D world
     let player_forward = &player_query.single().0.up();
     for (mut transform, mut velocity) in &mut player_query {
@@ -45,7 +45,7 @@ fn move_player(
             transform.rotate_z(rotation * rotation_speed * time.delta_seconds());
         }
         if actions.player_thrust {
-            velocity.0 = *player_forward * translation_speed;
+            velocity.0 += *player_forward * thrust_force;
         }
     }
 }
